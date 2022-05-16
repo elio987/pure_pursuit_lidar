2656259144b837b4fca04666ff57881c06ff0eea
elio987 <leo_gracidam@hotmail.com>
2022-05-15 23:32:44 -0500

    Final comentado

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # pure_pursuit_lidar
+
+- Para poder correr el codigo corre:
+
+```
+roslaunch puzzlebot_gazebo puzzlebot_room.launch
+```
+- Despues en una nueva terminal:
+
+```
+roslaunch puzzlebot_nav2d puzzlebot_navigation.launch
+```
+- En una nueva terminal:
+```
+roslaunch puzzlebot_nav2d pure_pursuit.launch
+```
+- En caso de querer ingresar nuevos waypoints ve al archivo:
+```
+/puzzlebot_nav2d/config/waypoints.yalm
+```
+- En ese archivo mete los dos componentes de los waypoints separados por una coma. Ejemplo:
+```
+waypoints: [0, 0, 0, 4, 2, 5, 4, 2, 5, 1, 7, 1, 8.5, 3.5, 7, 7]
+```
+- Arbol usado:
+![Alt text](PyTree.drawio.png?raw=true "Behaviour Tree")
--- puzzlebot_nav2d/src/pure_persuit_final.py
+++ puzzlebot_nav2d/src/pure_persuit_final.py
@@ -47,35 +47,12 @@ def go_to_point_controller(x, y, vmax, Kth, alpha):
     if y == -0.0:
         y = 0.0"""
     w = Kth * np.arctan2(y,x)
-    #print(np.arctan2(np.round(y,2),np.round(x,2)))
     d = np.sqrt(x**2 + y**2)
     v = vmax*(1-np.exp(-alpha*(d**2)))
     #---------------------------------------------------------
 
     return w, v, d
-"""
-def steer_towards_point_controller(x, y, v):
-    Given an intermediate goal point and a (constant) linear velocity, calculates the
-    angular velocity that will steer the robot towards the goal such that the robot
-    moves in a circular arc that passes through the intermediate goal point.
 
-    Arguments
-    ---------
-    x     :  float
-      The x-coordinate of the goal in the reference frame of the robot, in m
-    y     :  float
-      The x-coordinate of the goal in the reference frame of the robot, in m
-    v     :  float
-      The linear velocity in m/s
-
-    Returns
-    ---------
-    w   :  float
-      The angular velocity in rad/s
-
-    #---------------------------------------------------------
-    # YOUR CODE HERE
-"""
 def steer_towards_point_controller(x, y, v):
     """
     Given an intermediate goal point and a (constant) linear velocity, calculates the
@@ -112,7 +89,6 @@ def steer_towards_point_controller(x, y, v):
     """
     L = x**2 + y**2
     fi = (2*y)/L
-    #print("fi ",fi)
     w = np.round(fi * v,2)
     #---------------------------------------------------------
 
@@ -239,25 +215,20 @@ class Go2Point(py_trees.behaviour.Behaviour):
         self.goal.header.stamp = rospy.Time(0)
         print(self.goal.point.x,self.goal.point.y)
         goal_b = self.tf_buffer.transform(self.goal,self.robot_frame,timeout=rospy.Duration(10.0))
-        #print(self.goal)
-        #print(goal_b)
         #----------------------------------------------------
 
         w, v, dist = go_to_point_controller(goal_b.point.x, goal_b.point.y,
                                             self.vel_max, self.K_theta, self.alpha)
-        #print("resta ", dist,self.dist)
 
         if dist < self.dist:
             # Within the desired distance, so return success
             return py_trees.Status.SUCCESS
-            #print("com")
         else:
             #----------------------------------------------------
             # YOUR CODE HERE
             # Publish the velocity command to cmd_vel
             self.msg.linear.x = v # etc.
             self.msg.angular.z = w
-            #print("cor")
             self.cmd_vel_pub.publish(self.msg)
             #----------------------------------------------------
 
@@ -344,14 +315,12 @@ class PursuitGoal(py_trees.behaviour.Behaviour):
         self.wp1.header.stamp = rospy.Time(0)
         wp0_b = self.tf_buffer.transform(self.wp0,self.robot_frame,timeout=rospy.Duration(10.0))
         wp1_b = self.tf_buffer.transform(self.wp1,self.robot_frame,timeout=rospy.Duration(10.0))
-        #print("wx ",wp0_b.point.x,wp1_b.point.x)
-        #print("wy ",wp0_b.point.y,wp1_b.point.y)
         #------------------------------------------------------------
 
         pg, beta = get_goal_point([wp0_b.point.x, wp0_b.point.y],
                                   [wp1_b.point.x, wp1_b.point.y],
                                   self.L)
-        #print("b ",beta)
+        #Distance from the robot to the next waypoint
         error = np.sqrt(((pg[0]-wp1_b.point.x)**2)+((pg[1]-wp1_b.point.y)**2))
         if (beta > 1.0) and (error < 0.05):
             # Succeeded in moving along the trajectory from current to next waypoint
@@ -369,11 +338,7 @@ class PursuitGoal(py_trees.behaviour.Behaviour):
             return py_trees.Status.FAILURE
 
         else:
-            #print("pg: ",pg[0],pg[1])
-            #print(pg)
-            #print([wp0_b.point.x, wp0_b.point.y])
             w = steer_towards_point_controller(pg[0], pg[1], self.vel_max)
-            #print("w ",w)
             #----------------------------------------------------
             # YOUR CODE HERE
             # Publish the velocity command to cmd_vel
@@ -393,117 +358,66 @@ class PursuitGoal(py_trees.behaviour.Behaviour):
 
 class CarAtGoal(py_trees.behaviour.Behaviour):
     """
-    Takes two waypoints (current and next) and goes toward a point on
-    the line joining the two waypoints and at the look-ahead distance.
-    Returns SUCCESS when the next intermediate goal point is past the
-    next waypoint.
-    Returns RUNNING when still moving along the trajectory between the
-    two waypoints.
-    Returns FAILURE if no point on the trajectory is at the
-    look-ahead distance from the robot.
+    Takes the last waypoints and gets the transformation from the current point to the last point,
+    if we are close to the last point we stop the car and return SUCCESS, if we are not near the point we return
+    FAILURE
     """
 
     def __init__(self, name="CarAtGoal"):
-        """
-        Default construction.
-        """
         super(CarAtGoal, self).__init__(name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def setup(self,wpf,tf_buffer,cmd_vel_pub, robot_frame='base_link'):
+        #Lastwaypoint
         self.wpf = wpf
         self.tf_buffer = tf_buffer
+        #Frame of the robot
         self.robot_frame = robot_frame
+        #Velocity control
         self.cmd_vel_pub = cmd_vel_pub
         self.msg = geometry_msgs.msg.Twist()
         return True
 
     def initialise(self):
-        """
-        Since simple proportional control, no initialization needed. In case
-        of PI, PD, or PID, the controller would have some state that should
-        be initialized here.
-        """
         pass
 
     def update(self):
-        """
-        Do the work:
-        1) Transform waypoints to robot-centric frame
-        2) Compute the control signal
-        3) Publish the control signal
-        """
-
-        #------------------------------------------------------------
-        # YOUR CODE HERE
-        # Transform the two waypoints to the robot reference frame
+        #We get the transformation from the robot to the last waypoint
         self.wpf.header.stamp = rospy.Time(0)
         error = self.tf_buffer.transform(self.wpf,self.robot_frame,timeout=rospy.Duration(10.0))
+        #We get the euclidean from the robot to the last waypoint
         error = np.sqrt(((error.point.x)**2)+((error.point.y)**2))
-        #print("wx ",wp0_b.point.x,wp1_b.point.x)
-        #print("wy ",wp0_b.point.y,wp1_b.point.y)
-        #------------------------------------------------------------
-        #print("b ",beta)
-        print("error ",error)
+        #If "error is bigger than the tolerance we are not at the current waypoint"
         if error > 0.45:
-            print("lejos")
             return py_trees.Status.FAILURE
+        #If we are near the last waypoint we stop the car and send SUCCESS
         else:
-            print("cerca")
             self.msg.linear.x = 0
             self.msg.angular.z = 0
             self.cmd_vel_pub.publish(self.msg)
             return py_trees.Status.SUCCESS
     def terminate(self, new_status):
-        """
-        Nothing to clean up
-        """
         pass
 
 class Fallback(py_trees.behaviour.Behaviour):
     """
-    Takes two waypoints (current and next) and goes toward a point on
-    the line joining the two waypoints and at the look-ahead distance.
-    Returns SUCCESS when the next intermediate goal point is past the
-    next waypoint.
-    Returns RUNNING when still moving along the trajectory between the
-    two waypoints.
-    Returns FAILURE if no point on the trajectory is at the
-    look-ahead distance from the robot.
+    If we are at the waypoint and the real distance from the car to the real waypoint
+    is bigger than the tolerance we go backward until we are inside the tolerance.
+    We get the next waypoint and frame from the robot.
     """
 
     def __init__(self, name="Fallback"):
-        """
-        Default construction.
-        """
         super(Fallback, self).__init__(name)
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
-    def setup(self, wpf,
-              cmd_vel_pub, tf_buffer, robot_frame='base_link',
-              vel_max=0.6):
-        """
-        Arguments
-        ---------
-          wp0        :  array-like (2,)
-             The current waypoint.
-          wp1        :  array-like (2,)
-             The next waypoint.
-          look_ahead_distance    :  float
-             The main parameter of the pure pursuit algorithm
-          cmd_vel_pub :  Publisher
-             Publisher that publishes to the /cmd_vel topic
-          tf_buffer   :  tf2_ros.Buffer
-             Transform buffer for transforming the waypoints.
-          robot_frame :  String
-             The name of reference frame of the robot.
-          vel_max     :  float
-             The maximum linear velocity to command.
-        """
+    def setup(self, wpf,cmd_vel_pub, tf_buffer, robot_frame='base_link',vel_max=0.6):
+        #The next waypoint
         self.wpf = wpf
         self.cmd_vel_pub = cmd_vel_pub
         self.tf_buffer = tf_buffer
+        #Frame from the robot
         self.robot_frame = robot_frame
+        #Velicuty of the robot
         self.vel_max = vel_max
 
         self.msg = geometry_msgs.msg.Twist()
@@ -511,36 +425,21 @@ class Fallback(py_trees.behaviour.Behaviour):
         return True
 
     def initialise(self):
-        """
-        Since simple proportional control, no initialization needed. In case
-        of PI, PD, or PID, the controller would have some state that should
-        be initialized here.
-        """
         pass
 
     def update(self):
-        """
-        Do the work:
-        1) Transform waypoints to robot-centric frame
-        2) Compute the control signal
-        3) Publish the control signal
-        """
-
-        #------------------------------------------------------------
-        # YOUR CODE HERE
-        # Transform the two waypoints to the robot reference frame
         self.wpf.header.stamp = rospy.Time(0)
+        #The transformation from the robot to the next waypoint
         wpf = self.tf_buffer.transform(self.wpf,self.robot_frame,timeout=rospy.Duration(10.0))
-        #print("wx ",wp0_b.point.x,wp1_b.point.x)
-        #print("wy ",wp0_b.point.y,wp1_b.point.y)
-        #------------------------------------------------------------
-        #print("b ",beta)
+        #The distance from the robot to the next waypoiny
         error = np.sqrt(((wpf.point.x)**2)+((wpf.point.y)**2))
+        #While we are not inside the tolerance we go backward
         if error > 0.05:
             self.msg.angular.z = 0.0
             self.msg.linear.x = -1*vel_max
             self.cmd_vel_pub.publish(self.msg)
             return py_trees.Status.RUNNING
+        #If we are inside the tolerance we go forward again
         else:
             self.msg.angular.z = 0.0
             self.msg.linear.x = vel_max
@@ -549,9 +448,6 @@ class Fallback(py_trees.behaviour.Behaviour):
 
 
     def terminate(self, new_status):
-        """
-        Nothing to clean up
-        """
         pass
 
 def create_behavior_tree(waypoints, frame, look_ahead_dist, vel_max,
@@ -565,11 +461,16 @@ def create_behavior_tree(waypoints, frame, look_ahead_dist, vel_max,
     tf_buffer = tf2_ros.Buffer()
     tf2_ros.TransformListener(tf_buffer)
     cmd_vel_pub = rospy.Publisher('/cmd_vel' , geometry_msgs.msg.Twist, queue_size=1 )
+    #We start the root of the tree with a Selector
     root = py_trees.composites.Selector("Root")
+    #We create a root of Sequence to insert the Pure Pursuit points, fallbacks and Go2Point
     go_points = py_trees.composites.Sequence("PurePursuit")
 
+    #We create the CarAtGoal node
     atGoal = CarAtGoal()
+    #We insert the last waypoint
     atGoal.setup(waypoints[-1],tf_buffer,cmd_vel_pub)
+    #We add the childrens
     root.add_children([atGoal,go_points])
 
     # Setup and add node handling the first waypoint. We should go towards this point
@@ -584,16 +485,17 @@ def create_behavior_tree(waypoints, frame, look_ahead_dist, vel_max,
     # Add the nodes to handle pairs of waypoints
     contador = 1
     for cwp_, nwp_ in zip(waypoints[0:-2], waypoints[1:-1]):
+        #We create a selector to insert the PurePursuit node and Fallback node
         nextPoint = py_trees.composites.Selector("Next_"+str(contador))
-        print(cwp_,nwp_)
         pg = PursuitGoal()
         pg.setup(cwp_, nwp_, look_ahead_dist, cmd_vel_pub,
                  tf_buffer, vel_max=vel_max)
         fail = Fallback()
         fail.setup(nwp_,cmd_vel_pub, tf_buffer,vel_max=vel_max)
+        #We insert selector
         go_points.add_child(nextPoint)
+        #We insert the nodes Fallback and PurePursuit
         nextPoint.add_children([pg,fail])
-        #go_points.add_child(pg)
         contador += 1
 
 
@@ -601,15 +503,9 @@ def create_behavior_tree(waypoints, frame, look_ahead_dist, vel_max,
     # YOUR CODE HERE
     # Add the final node to go to the last waypoint, which is the final goal point
     g2p = Go2Point()
-    p = PointStamped()
-    p.header.frame_id = rospy.get_param("/pure_pursuit/frame")
-    p.point.x = 2
-    p.point.y = 0
     g2p.setup(waypoints[-1], look_ahead_dist, cmd_vel_pub, tf_buffer,
               vel_max=vel_max, K_theta=K_theta, alpha=alpha)
     go_points.add_child(g2p)
-    #idle = py_trees.behaviours.Running(name = "Idle")
-    #root.add_child(idle)
     #
     #----------------------------------------------------------------------------
 
@@ -633,7 +529,6 @@ if __name__ == '__main__':
         p.header.frame_id = frame
         p.point.x = wp_[0]
         p.point.y = wp_[1]
-        #print(wp_[0],wp_[1])
         waypoints_ps.append(p)
 
     bt_tree = create_behavior_tree(waypoints_ps, frame, L, vmax,
